302f40c7b3a2dbf3199c5d029ba984397442ef54
Alvon Jovanus Chandra <alvonchan@gmail.com>
2023-12-01 14:38:23 +0700

    change tdd notification

--- src/test/e2e/users/getCurrentNotification.spec.ts
+++ src/test/e2e/users/getCurrentNotification.spec.ts
@@ -3,7 +3,7 @@ import { createApp } from '../../../app.js';
 import { deleteAllUsers } from '../../../infrastructure/database/mongodb/users/utils/deleteAllUsers.js';
 import { createFakeUser } from '../../../infrastructure/database/mongodb/users/utils/createFakeUser.js';
 
-describe('get current notifications example', () => {
+describe('get current user notifications example', () => {
   let app;
 
   afterAll(async () => {
@@ -19,7 +19,7 @@ describe('get current notifications example', () => {
     app = createApp();
   });
 
-  it('should be able get current notification', async () => {
+  it('should be able get current user notification', async () => {
     const data = await createFakeUser();
 
     const authResponse = await request(app)
@@ -33,32 +33,10 @@ describe('get current notifications example', () => {
     expect(response.body.status).toEqual('success');
     expect(response.body.notificationCount).toBeDefined();
 
-    expect(response.body.data[0]._id.toString()).toBeDefined();
-    expect(response.body.data[0].type).toBeDefined();
-    expect(response.body.data[0].message).toBeDefined();
-    expect(response.body.data[0].date).toBeDefined();
-  });
-
-  it('should be able to limit results', async () => {
-    const data = await createFakeUser();
-
-    const authResponse = await request(app)
-      .post(`/v1/users/login`)
-      .send({ email: data[0].email, password: '12345678' });
-    const { token } = authResponse.body;
-
-    const response = await request(app)
-      .get('/v1/users/notification')
-      .query({ limit: 1 })
-      .set('Authorization', `Bearer ${token}`);
-
-    // expect http response
-    expect(response.statusCode).toEqual(200);
-
-    // expect response json
-    expect(response.body.status).toEqual('success');
-    expect(response.body.results).toEqual(1);
-    expect(response.body.limit).toEqual(1);
+    expect(response.body.data.today).toBeDefined();
+    expect(response.body.data.yesterday).toBeDefined();
+    expect(response.body.data.thisWeek).toBeDefined();
+    expect(response.body.data.thisMonth).toBeDefined();
   });
 
   it('should thrown Authentication Error if user is not logged in', async () => {
